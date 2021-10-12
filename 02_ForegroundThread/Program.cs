6be033cf964e66d607f6cf8df1047c841d1eeb3a
arminatwork <workwitharmin7@gmail.com>
2021-10-12 16:44:21 +0330

    Add 02_Foreground Thread

---
+++ 02_ForegroundThread/Program.cs
@@ -0,0 +1,17 @@
+﻿//FOREGROUND THREAD EXAMPLE
+Thread progress = new(Test_ForegroundThread.SimpleProgress);
+progress.Start();
+Console.WriteLine("Main Thread ends!");
+
+public class Test_ForegroundThread
+{
+    public static void SimpleProgress()
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine("SimpleProgress in progress...");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine("SimpleProgress ends!");
+    }
+}
