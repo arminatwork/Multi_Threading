731e407f95db3f50dc760845584962fbf8d1ec2f
arminatwork <workwitharmin7@gmail.com>
2021-10-12 16:53:00 +0330

    Add 03_BackgroundThread

---
+++ 03_BackgroundThread/Program.cs
@@ -0,0 +1,25 @@
+﻿//BACKGROUND THREAD EXAMPLE
+Thread progress = new(Test_BackgroundThread.SimpleProgress);
+
+progress.Name = "TestThread";
+
+progress.Start();
+
+//allow thread to run in the background
+progress.IsBackground = true;
+
+Console.WriteLine("Main Thread ends!");
+
+public class Test_BackgroundThread
+{
+    public static void SimpleProgress()
+    {
+        //current thread name
+        Console.WriteLine("In Progress Thread is: {0}", Thread.CurrentThread.Name);
+
+        Thread.Sleep(3000);
+
+        //completed thread name
+        Console.WriteLine("Completed Thread is: {0}", Thread.CurrentThread.Name);
+    }
+}
